3b47aec30f1b047e5463c3088755b592c02ab3a8
Gage <gcm55@gmail.com>
2016-10-04 22:02:56 -0700

    Split raycast file into multiple header files and added some given code for parseing JSON

--- raycast.c
+++ raycast.c
@@ -1,220 +1,123 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include "raycast.h"
 
-/* A structure used to buffer the information
-    that is being read in from the JSON file */
-typedef struct json {
-	char type[8];
-	float color[3];
-	float width;
-	float height;
-	int radius;
-	int position[3];
-	int normal[3];
-} json;
+int main(int argc, char const *argv[]) {
 
-int main(int c, char** argv) {
-    read_scene(argv[3]);
-    return 0;
-}
-
-int line = 1;
-
-// next_c() wraps the getc() function and provides error checking and line
-// number maintenance
-int next_c(FILE* json) {
-    int c = fgetc(json);
-#ifdef DEBUG
-    printf("next_c: '%c'\n", c);
-#endif
-    if (c == '\n') {
-        line += 1;
-    }
-    if (c == EOF) {
-        fprintf(stderr, "Error: Unexpected end of file on line number %d.\n", line);
-        exit(1);
-    }
-    return c;
-}
-
-
-// expect_c() checks that the next character is d.  If it is not it emits
-// an error.
-void expect_c(FILE* json, int d) {
-    int c = next_c(json);
-    if (c == d) return;
-    fprintf(stderr, "Error: Expected '%c' on line %d.\n", d, line);
+  // Ensures the correct number of args were passed in
+  if (argc != 5) {
+    fprintf(stderr, "\nERROR: Excpected 5 arguments. %d were given.", argc);
     exit(1);
-}
+  }
+
+  // Checks width and height
+  if ((atoi(argv[1]) == 0 && *argv[1] != '0') ||
+      (*argv[2] != '0' && atoi(argv[2]) == 0)) {
+    fprintf(stderr, "\nERROR: Input height or width are not numerical.\n");
+    exit(1);
+  }
+
+  // Check that file exists
+  FILE *inFile = fopen(argv[3], "r");
+  if (!inFile) {
+    fprintf(stderr, "\nERROR: Input file does not exist.\n");
+    exit(1);
+  }
+
+  // Creates pixel buffer
+  int M = atoi(argv[2]);
+  int N = atoi(argv[1]);
+  pixel *pixBuffer = (pixel *) malloc(sizeof(pixel) * N * M);
+  fill(pixBuffer, &N, &M);
 
 
-// skip_ws() skips white space in the file.
-void skip_ws(FILE* json) {
-    int c = next_c(json);
-    while (isspace(c)) {
-        c = next_c(json);
-    }
-    ungetc(c, json);
-}
+  // finds the size of the file
+  fseek(inFile, 0L, SEEK_END);
+  int bufferSize = ftell(inFile);
+  char *buffer = (char *) malloc(sizeof(char) * bufferSize);
+  entity *entities = (entity *) malloc(sizeof(entity) * 128 + 1);
+  memset(buffer, 0, sizeof(buffer));
+  rewind(inFile);
 
+  // Read in the file to the buffer
+  read_buffer(inFile, buffer, &bufferSize);
+  fclose(inFile);
 
-// next_string() gets the next string from the file handle and emits an error
-// if a string can not be obtained.
-char* next_string(FILE* json) {
-    char buffer[129];
-    int c = next_c(json);
-    if (c != '"') {
-        fprintf(stderr, "Error: Expected string on line %d.\n", line);
-        exit(1);
-    }
-    c = next_c(json);
-    int i = 0;
-    while (c != '"') {
-        if (i >= 128) {
-            fprintf(stderr, "Error: Strings longer than 128 characters in length are not supported.\n");
+  // Read in the scene from the buffer
+  int objCount = read_scene(buffer, &bufferSize, entities);
+  free(buffer);
+
+  // If no camera or entities were supplied, write a blank file
+  if (!objCount) {
+    FILE *outFile = fopen(argv[4], "w");
+    fclose(outFile);
+    printf("No camera or entities supplied. File will be written as blank.");
+    return 1;
+  }
+
+  // raycast and write to scene
+  double cx = 0;
+  double cy = 0;
+  double h = entities[0].camera.height;
+  double w = entities[0].camera.width;
+  double pixHeight = h / M;
+  double pixWidth = w / N;
+  int y, x;
+  int i;
+  int j = N * M;
+
+  // This loop writes the scene to the file
+  for (y = 0; y < M; y++) {
+    for (x = 0; x < N; x++) {
+      double rO[3] = {0, 0, 0};
+      double rD[3] = {
+        cx - (w/2) + pixWidth * (x + 0.5),
+        cy - (h/2) + pixHeight * (y + 0.5),
+        1
+      };
+      normal(rD);
+
+      double closest = INFINITY;
+
+      for (i = 1; i < objCount; i++) {
+        double t = 0;
+        int type = entities[i].type;
+        switch(type) {
+          case 2:
+            t = intersect_sphere(rO, rD,
+                                entities[i].sphere.radius,
+                                entities[i].sphere.position);
+            break;
+          case 3:
+            t = intersect_plane(rO, rD,
+                               entities[i].plane.normal,
+                               entities[i].plane.position);
+            break;
+          default:
             exit(1);
         }
-        if (c == '\\') {
-            fprintf(stderr, "Error: Strings with escape codes are not supported.\n");
-            exit(1);
+        if (t > 0 && t < closest) {
+          closest = t;
+          // Account for intersection and write color accordingly
+          if (type == 2) {
+            pixBuffer[j].r = 255 * (entities[i].sphere.color[0]);
+            pixBuffer[j].g = 255 * (entities[i].sphere.color[1]);
+            pixBuffer[j].b = 255 * (entities[i].sphere.color[2]);
+          } else {
+            pixBuffer[j].r = 255 * (entities[i].plane.color[0]);
+            pixBuffer[j].g = 255 * (entities[i].plane.color[1]);
+            pixBuffer[j].b = 255 * (entities[i].plane.color[2]);
+          }
         }
-        if (c < 32 || c > 126) {
-            fprintf(stderr, "Error: Strings may contain only ascii characters.\n");
-            exit(1);
-        }
-        buffer[i] = c;
-        i += 1;
-        c = next_c(json);
+      }
+      --j;
     }
-    buffer[i] = 0;
-    return strdup(buffer);
-}
+  }
 
-double next_number(FILE* json) {
-    double value;
-    fscanf(json, "%f", &value);
-    // Error check this..
-    return value;
-}
+  // write to file
+  FILE *outFile = fopen(argv[4], "wb");
+  int ver = 6;
+  int colorMax = 255;
+  write6(outFile, pixBuffer, &N, &M, &colorMax, &ver);
+  fclose(outFile);
 
-double* next_vector(FILE* json) {
-    double* v = malloc(3*sizeof(double));
-    expect_c(json, '[');
-    skip_ws(json);
-    v[0] = next_number(json);
-    skip_ws(json);
-    expect_c(json, ',');
-    skip_ws(json);
-    v[1] = next_number(json);
-    skip_ws(json);
-    expect_c(json, ',');
-    skip_ws(json);
-    v[2] = next_number(json);
-    skip_ws(json);
-    expect_c(json, ']');
-    return v;
-}
-
-
-void read_scene(char* filename) {
-    int c;
-    FILE* json = fopen(filename, "r");
-
-    if (json == NULL) {
-        fprintf(stderr, "Error: Could not open file \"%s\"\n", filename);
-        exit(1);
-    }
-
-    skip_ws(json);
-
-    // Find the beginning of the list
-    expect_c(json, '[');
-
-    skip_ws(json);
-
-    // Find the objects
-
-    while (1) {
-        c = fgetc(json);
-        if (c == ']') {
-            fprintf(stderr, "Error: This is the worst scene file EVER.\n");
-            fclose(json);
-            return;
-        }
-        if (c == '{') {
-            skip_ws(json);
-
-            // Parse the object
-            char* key = next_string(json);
-            if (strcmp(key, "type") != 0) {
-                fprintf(stderr, "Error: Expected \"type\" key on line number %d.\n", line);
-                exit(1);
-            }
-
-            skip_ws(json);
-
-            expect_c(json, ':');
-
-            skip_ws(json);
-
-            char* value = next_string(json);
-
-            if (strcmp(value, "camera") == 0) {
-            } else if (strcmp(value, "sphere") == 0) {
-            } else if (strcmp(value, "plane") == 0) {
-            } else {
-                fprintf(stderr, "Error: Unknown type, \"%s\", on line number %d.\n", value, line);
-                exit(1);
-            }
-
-            skip_ws(json);
-
-            while (1) {
-                // , }
-                c = next_c(json);
-                if (c == '}') {
-                    // stop parsing this object
-                    break;
-                } else if (c == ',') {
-                    // read another field
-                    skip_ws(json);
-                    char* key = next_string(json);
-                    skip_ws(json);
-                    expect_c(json, ':');
-                    skip_ws(json);
-                    if ((strcmp(key, "width") == 0) ||
-                        (strcmp(key, "height") == 0) ||
-                        (strcmp(key, "radius") == 0)) {
-                        double value = next_number(json);
-                    } else if ((strcmp(key, "color") == 0) ||
-                               (strcmp(key, "position") == 0) ||
-                               (strcmp(key, "normal") == 0)) {
-                        double* value = next_vector(json);
-                    } else {
-                        fprintf(stderr, "Error: Unknown property, \"%s\", on line %d.\n",
-                                key, line);
-                        //char* value = next_string(json);
-                    }
-                    skip_ws(json);
-                } else {
-                    fprintf(stderr, "Error: Unexpected value on line %d\n", line);
-                    exit(1);
-                }
-            }
-            skip_ws(json);
-            c = next_c(json);
-            if (c == ',') {
-                // noop
-                skip_ws(json);
-            } else if (c == ']') {
-                fclose(json);
-                return;
-            } else {
-                fprintf(stderr, "Error: Expecting ',' or ']' on line %d.\n", line);
-                exit(1);
-            }
-        }
-    }
+  return 0;
 }
